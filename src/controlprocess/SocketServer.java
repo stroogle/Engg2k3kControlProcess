feca97a4f0425c740ecc995454455e01fbd8e820
James Pirrie <jamespirrie@jamess-air.lan>
2024-08-20 17:28:07 +1000

    Added sendMessage(), implemented for PrintWriter and DataOutputStream approach

--- src/controlprocess/SocketServer.java
+++ src/controlprocess/SocketServer.java
@@ -8,6 +8,7 @@ public class SocketServer {
     private ServerSocket server;
     private Socket client;
     private PrintWriter output;
+    private DataOutputStream output2;
     private BufferedReader input;
     private int port;
 
@@ -20,6 +21,7 @@ public class SocketServer {
         client = server.accept();
 
         output = new PrintWriter(client.getOutputStream(), true);
+        // output2 = new DataOutputStream(client.getOutputStream());
         input = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
         String nextLine = input.readLine();
@@ -27,11 +29,13 @@ public class SocketServer {
         while (nextLine != null) {
 
             if (nextLine.equals("HELLO")) {
-                output.println("HI THERE");
+                sendMessage("HI THERE");
+                // sendMessage2("HI THERE");
             }
 
             if (nextLine.equals("STOP")) {
-                output.println("STOP");
+                sendMessage("STOP");
+                // sendMessage2("HI THERE");
                 break;
             }
 
@@ -44,11 +48,34 @@ public class SocketServer {
 
     public void close() throws Exception {
 
+        // output2.close();
+
         this.input.close();
         this.output.close();
         this.client.close();
         this.server.close();
 
     }
+
+    // PrintWriter variant.
+    public void sendMessage(String str) {
+        output.println(str);
+        System.out.println("SENT: " + str);
+         
+    }
+    
+
+    // DataOutputStream variant.
+    // public void sendMessage2(String str) {
+    //     try{
+    //         String msgWithNewLnChar = str + "\n";
+    //         output2.write(msgWithNewLnChar.getBytes());
+    //         output2.flush();
+    //         System.out.println("SENT: " + str);
+    //     } 
+    //     catch(Exception e){
+    //         System.out.println(e);
+    //     }
+    // }
 
 }
