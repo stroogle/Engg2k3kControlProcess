ff9010c792870742f7e6458aa14ccd8b0144f5cb
James Pirrie <james.pirrie@students.mq.edu.au>
2024-08-27 09:23:51 +1000

    Removed DataOutputStream versions

--- src/controlprocess/SocketServer.java
+++ src/controlprocess/SocketServer.java
@@ -8,7 +8,6 @@ public class SocketServer {
     private ServerSocket server;
     private Socket client;
     private PrintWriter output;
-    // private DataOutputStream output2;
     private BufferedReader input;
     private int port;
 
@@ -21,7 +20,6 @@ public class SocketServer {
         client = server.accept();
 
         output = new PrintWriter(client.getOutputStream(), true);
-        // output2 = new DataOutputStream(client.getOutputStream());
         input = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
         String nextLine = input.readLine();
@@ -30,12 +28,10 @@ public class SocketServer {
 
             if (nextLine.equals("HELLO")) {
                 sendMessage("HI THERE");
-                // sendMessage2("HI THERE");
             }
 
             if (nextLine.equals("STOP")) {
                 sendMessage("STOP");
-                // sendMessage2("HI THERE");
                 break;
             }
 
@@ -47,35 +43,18 @@ public class SocketServer {
     }
 
     public void close() throws Exception {
-
-        // output2.close();
-
         this.input.close();
         this.output.close();
         this.client.close();
         this.server.close();
 
     }
-
-    // PrintWriter variant.
+    
     public void sendMessage(String str) {
         output.println(str);
         System.out.println("SENT: " + str);
          
     }
-    
 
-    // DataOutputStream variant.
-    // public void sendMessage2(String str) {
-    //     try{
-    //         String msgWithNewLnChar = str + "\n";
-    //         output2.write(msgWithNewLnChar.getBytes());
-    //         output2.flush();
-    //         System.out.println("SENT: " + str);
-    //     } 
-    //     catch(Exception e){
-    //         System.out.println(e);
-    //     }
-    // }
 
 }
